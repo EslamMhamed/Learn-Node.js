1bc0b94ab1f84cd86fcf8c0ee864afb3b3ca6894
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-10-06 22:42:18 +0300

    http -app example

--- app.js
+++ app.js
@@ -23,7 +23,10 @@
 const http = require("http")
 const {readFileSync} = require("fs")
 
-const homePage = readFileSync("./index.html")
+const homePage = readFileSync("./express-tutorial/navbar-app/index.html")
+const homeStyles = readFileSync("./express-tutorial/navbar-app/styles.css")
+const homeImage = readFileSync("./express-tutorial/navbar-app/logo.svg")
+const homeLogic = readFileSync("./express-tutorial/navbar-app/browser-app.js")
 
 const server = http.createServer((req, res) =>{
     const url = req.url
@@ -31,11 +34,20 @@ const server = http.createServer((req, res) =>{
         res.writeHead(200, {"content-type" : "text/html"})
         res.write(homePage)
         res.end()
-    }else if(url === "/about"){
-        res.writeHead(200, {"content-type" : "text/html"})
-        res.write("<h1>About Page</h1>")
+    }else if(url === "/styles.css"){
+        res.writeHead(200, {"content-type" : "text/css"})
+        res.write(homeStyles)
         res.end()
-    }else{
+    }else if(url === "/logo.svg"){
+        res.writeHead(200, {"content-type" : "image/svg+xml"})
+        res.write(homeImage)
+        res.end()
+    }else if(url === "/browser-app.js"){
+        res.writeHead(200, {"content-type" : "text/javascript"})
+        res.write(homeLogic)
+        res.end()
+    }
+    else{
         res.writeHead(404, {"content-type" : "text/html"})
         res.write("<h1>Not Found Page</h1>")
         res.end()
