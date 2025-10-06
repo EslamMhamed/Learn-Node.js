17584776a65e37f7483d6c91ed2fbd9feb5bb030
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-10-06 22:22:10 +0300

    html file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // const http = require("http")
 
+
 // const server = http.createServer((req, res) =>{
 //     console.log("user hit the server")
 //     res.end("home page")
@@ -20,12 +21,15 @@
 // server.listen(5000)
 
 const http = require("http")
+const {readFileSync} = require("fs")
+
+const homePage = readFileSync("./index.html")
 
 const server = http.createServer((req, res) =>{
     const url = req.url
     if(url === "/"){
         res.writeHead(200, {"content-type" : "text/html"})
-        res.write("<h1>Home Page</h1>")
+        res.write(homePage)
         res.end()
     }else if(url === "/about"){
         res.writeHead(200, {"content-type" : "text/html"})
