0d90ff221d9a63042485e28a8fed32d027da4cff
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-10-06 15:27:18 +0300

    stream-http

---
+++ 18-steams-http.js
@@ -0,0 +1,14 @@
+const fs = require("fs") 
+const http = require("http") 
+
+http.createServer((req, res)=>{
+    const fileStream = fs.createReadStream("./content/big.txt", "utf8")
+
+    fileStream.on("open",()=>{
+        fileStream.pipe(res)
+    } )
+
+    fileStream.on("error",(err)=>{
+        res.end(err)
+    } )
+}).listen(5000)
