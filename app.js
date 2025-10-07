8e6491b27ac20702e0e1db06e2d0c571fa41d1b8
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-10-07 23:58:00 +0300

    express-router

--- app.js
+++ app.js
@@ -1,52 +1,20 @@
 const express = require("express")
 const app = express()
 
-const {people} = require("./data")
+const people = require("./express-tutorial/routes/people")
+const auth = require("./express-tutorial/routes/auth")
 
 app.use(express.static("./express-tutorial/methods-public"))
 
 app.use(express.urlencoded({extended:false}))
 
-app.get("/api/people", (req,res)=>{
-    res.status(201).json({success: true, data: people})
-})
+app.use(express.json())
 
-app.put("/api/people/:id", (req,res)=>{
-    const {id} = req.params
-    const {name} = req.body
-    const person = people.find(person => person.id === Number(id))
-    if(!person){
-        res.status(404).json({success: false, msg :`no person with id ${id}`})
-    }
-    const newPeople = people.map(person=> {
-        if(person.id === Number(id)){
-            person.name = name
-        }
-        return person
-    })
-    res.status(201).json({success: true, data: newPeople})
-})
+app.use("/api/people", people)
+app.use("/login", auth)
 
-app.delete("/api/people/:id", (req,res)=>{
-    const {id} = req.params
-    const person = people.find(person => person.id === Number(id))
 
-    if(!person){
-        res.status(404).json({success : false, msg: `no person with id : ${id}`})
-    }
-    const newPeople = people.filter(person => person.id !== Number(id))
 
-   return res.status(201).json({success: true , data: newPeople})
-})
-
-app.post("/login", (req, res)=>{
-    const {name} = req.body
-    if(name){
-        res.status(200).send(`Welcome ${name}`)
-    }else{
-        res.status(401).send("Please Provide Credentails")
-    }
-})
 
 app.listen(5000, ()=>{
     console.log("Server listening on port 5000...")
