b2f4b0044b35620f7b8cd324f791715792ac6c69
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-10-07 12:27:25 +0300

    update

--- express-tutorial/4-params-query.js
+++ express-tutorial/4-params-query.js
@@ -13,7 +13,7 @@ app.get("/api/v1/query", (req, res)=>{
     if(limit){
         sortedProducts= sortedProducts.slice(0, Number(limit))
     }
-    if(sortedProducts< 1){
+    if(sortedProducts.length< 1){
         // res.status(200).send("No Products matched your search")
         res.status(200).json({success: true, data: []})
     }
