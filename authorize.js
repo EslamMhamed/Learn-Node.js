e27459349341c7403c7f31f537960c2bf11dcd0c
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-10-07 14:49:26 +0300

    multi middleware function

---
+++ authorize.js
@@ -0,0 +1,11 @@
+const authorize = (req, res, next)=>{
+    const {user} = req.query
+    if(user === "eslam"){
+        req.user = {name: "eslam", id: 2}
+        next()
+    }else{
+        res.status(401).send("Unauthorized")
+    }
+}
+
+module.exports= authorize
