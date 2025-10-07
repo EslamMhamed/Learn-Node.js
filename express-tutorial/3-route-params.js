ae8b239a86343513078974d0fe657303bfe43444
Eslam AbdelWahab <mohaeslam22@gmail.com>
2025-10-07 11:40:30 +0300

    route-params

---
+++ express-tutorial/3-route-params.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express()
+const {products} = require("./data")
+
+app.get("/", (req, res)=>{
+    res.send('<h1>Home Page</h1><a href="/api/products" >Products</a>')
+})
+
+app.get("/api/products", (req, res)=>{
+    const newProducts= products.map(product => {
+        const {id, name, image}= product
+        return {id, name, image}
+    })
+    res.json(newProducts)
+})
+
+app.get("/api/products/:productId", (req, res)=>{
+    const {productId} = req.params
+    const singleProduct = products.find(product => product.id === Number(productId))
+    if(!singleProduct){
+        return res.status(404).send("Product does Not Exist")
+    }
+    res.json(singleProduct)
+})
+
+app.use((req, res)=>{
+    res.status(404).send("Page Not Found")
+})
+
+app.listen(5000, ()=>{
+    console.log("Server is listening on port 5000...")
+})
